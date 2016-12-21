c30ab0929d8c8327c52bb381898fdaaf0fab7998
donmaiq <kayhty@windowslive.com>
2016-12-21 10:56:27 +0200

    Update playlistmanager.lua

--- playlistmanager.lua
+++ playlistmanager.lua
@@ -267,8 +267,7 @@ end
 function playlist()
     local popen=nil
     if settings.linux_over_windows then
-        popen = io.popen('find '..search..' -type f -printf "%f\\n" 2>/dev/null') --linux version, not tested, if it doesn't work fix it to print filenames only 1 per row
-        --print('find '..search..' -type f -printf "%f\\n"')
+        popen = io.popen('find '..search..' -maxdepth 1 -type f -printf "%f\\n" 2>/dev/null | sort -f')
     else
         popen = io.popen('dir /b '..search) --windows version
     end
