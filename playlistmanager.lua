9eca244ae31db06590a75f9021ac79c6aaa22107
Jonnie <kayhty@windowslive.com>
2017-07-18 12:08:51 +0300

    fix sort case sensitivity

--- playlistmanager.lua
+++ playlistmanager.lua
@@ -388,8 +388,8 @@ function alphanumsort(o)
   local function padnum(d) local dec, n = string.match(d, "(%.?)0*(.+)")
     return #dec > 0 and ("%.12f"):format(d) or ("%s%03d%s"):format(dec, #n, n) end
     table.sort(o, function(a,b)
-    return tostring(a):gsub("%.?%d+",padnum)..("%3d"):format(#b)
-         < tostring(b):gsub("%.?%d+",padnum)..("%3d"):format(#a) end)
+    return tostring(a):lower():gsub("%.?%d+",padnum)..("%3d"):format(#b)
+         < tostring(b):lower():gsub("%.?%d+",padnum)..("%3d"):format(#a) end)
   return o
 end
 
