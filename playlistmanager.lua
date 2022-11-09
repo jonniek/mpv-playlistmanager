7e57bdd0954ec93e884282b1126acf66bd312362
Jonniek <kayhty@windowslive.com>
2022-11-09 17:36:58 +0900

    skip on_load for playlist files, fixes #92

--- playlistmanager.lua
+++ playlistmanager.lua
@@ -270,8 +270,8 @@ update_opts({filename_replace = true, loadfiles_filetypes = true})
 function on_loaded()
   filename = mp.get_property("filename")
   path = mp.get_property('path')
-  if utils.readdir(path, "dirs") then
-    -- a directory has been loaded, let's not do anything as mpv will expand it into files
+  if filename:match(".m3u$") or utils.readdir(path, "dirs") then
+    -- a directory or playlist has been loaded, let's not do anything as mpv will expand it into files
     return
   end
   --if not a url then join path with working directory
