0e0c16a37ca356aadba1bcf268ff32e32569549e
Jameson <108906685+2084x@users.noreply.github.com>
2022-12-11 23:18:55 +1100

    nil

--- playlistmanager.lua
+++ playlistmanager.lua
@@ -497,7 +497,7 @@ function draw_playlist()
   local h = 360
   local w = h * a
 
-  if settings.curtain_opacity ~= 0 and settings.curtain_opacity < 1.0 then
+  if settings.curtain_opacity ~= nil and settings.curtain_opacity ~= 0 and settings.curtain_opacity < 1.0 then
   -- curtain dim from https://github.com/christoph-heinrich/mpv-quality-menu/blob/501794bfbef468ee6a61e54fc8821fe5cd72c4ed/quality-menu.lua#L699-L707
     local alpha = 255 - math.ceil(255 * settings.curtain_opacity)
     ass.text = string.format('{\\pos(0,0)\\rDefault\\an7\\1c&H000000&\\alpha&H%X&}', alpha)
